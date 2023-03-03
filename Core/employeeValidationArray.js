8f1ebc3d0d46cb3b9aab1ce87d6e5303501e22b1
nabila_ahmed <nabilaahmed2022@gmail.com>
2023-03-03 16:35:01 +0200

    emp validation commit

---
+++ Core/employeeValidationArray.js
@@ -0,0 +1,54 @@
+const { body } = require("express-validator");
+exports.validatePostArray = [
+  body("_id").isInt().withMessage("Employee id should be an integer"),
+  body("fname")
+    .isString()
+    .withMessage("Employee first name should be string")
+    .isLength({ min: 2 })
+    .withMessage("Employee first name should contain at least 2 chars"),
+  body("lname")
+    .isString()
+    .withMessage("Employee last name should be string")
+    .isLength({ min: 2 })
+    .withMessage("Employee last name should contain at least 2 chars"),
+  body("email").isEmail().withMessage("invalid mail"),
+  body("password")
+    .isStrongPassword()
+    .withMessage(
+      "password should contains at least 8 characters, one uppercase letter, one lowercase letter,one special char, and one number"
+    ),
+  body("salary").isNumeric().withMessage("salary should be numeric value"),
+  body("birthdate").isDate().withMessage("invalid birth date"),
+  body("hiredate").isDate().withMessage("invalid hire date"),
+];
+exports.validatePatchArray = [
+  body("_id").isInt().withMessage("Employee id should be an integer"),
+  body("fname")
+    .optional()
+    .isString()
+    .withMessage("Employee first name should be string")
+    .isLength({ min: 2 })
+    .withMessage("Employee first name should contain at least 2 chars"),
+  body("lname")
+    .optional()
+    .isString()
+    .withMessage("Employee last name should be string")
+    .isLength({ min: 2 })
+    .withMessage("Employee last name should contain at least 2 chars"),
+  body("email").optional().isEmail().withMessage("invalid mail"),
+  body("password")
+    .optional()
+    .isStrongPassword()
+    .withMessage(
+      "password should contains at least 8 characters, one uppercase letter, one lowercase letter,one special char, and one number"
+    ),
+  body("salary")
+    .optional()
+    .isNumeric()
+    .withMessage("salary should be numeric value"),
+  body("birthdate").optional().isDate().withMessage("invalid birth date"),
+  body("hiredate").optional().isDate().withMessage("invalid hire date"),
+];
+exports.validateDelArray= [
+    body("_id").isInt().withMessage("Employee id should be an integer")
+];
