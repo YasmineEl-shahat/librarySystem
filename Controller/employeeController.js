a1a000ad46a5b459e101971c1e2b0202707aaeeb
nabila_ahmed <nabilaahmed2022@gmail.com>
2023-03-07 21:21:11 +0200

    search

--- Controller/employeeController.js
+++ Controller/employeeController.js
@@ -54,9 +54,11 @@ exports.updateEmployee = async (request , response , next)=>{
     catch(error){
         next(new Error("image not found " ))
     }
-
-     //update with role admin
-    if(request.role == "admin" ){
+    if(request.role=="employee"){
+        delete request.body.email
+        delete request.body.hiredate
+        delete request.body.salary
+    }
         employeeSchema.updateOne(
             {_id: request.body._id},
             { 
@@ -77,41 +79,41 @@ exports.updateEmployee = async (request , response , next)=>{
             else response.status(200).json({data})
         })
         .catch(error => next(error))
-    }
+    
     //update with role employee
-    else if (request.role == "employee"){
-    if(request.body.email !=null){
-        next(new Error("you can't update your email "));
-    }
-    else if(request.body.hiredate !=null){
-        next(new Error("you can't update your hiredate "));
-    }
-    else if(request.body.salary !=null){
-        next(new Error("you can't update your salary "));
-    }
-    else{
-    employeeSchema.updateOne(
-        {_id: request.body._id},
-        { 
-            $set: {
-                fname:request.body.fname,
-                lname:request.body.lname,
-                // password:hash,
-                birthdate: request.body.birthdate,
-                image: request.file?.path ? request.file.path : ""
-            }
-        }
-    ).then( (data) => {
-        if(data.matchedCount == 0)
-            next(new Error("employee not found for update"))
-        else response.status(200).json({data})
-    })
-    .catch(error => next(error))
-}
-}
-else {
-    next(Error);
-}
+//     else if (request.role == "employee"){
+//     if(request.body.email !=null){
+//         next(new Error("you can't update your email "));
+//     }
+//     else if(request.body.hiredate !=null){
+//         next(new Error("you can't update your hiredate "));
+//     }
+//     else if(request.body.salary !=null){
+//         next(new Error("you can't update your salary "));
+//     }
+//     else{
+//     employeeSchema.updateOne(
+//         {_id: request.body._id},
+//         { 
+//             $set: {
+//                 fname:request.body.fname,
+//                 lname:request.body.lname,
+//                 // password:hash,
+//                 birthdate: request.body.birthdate,
+//                 image: request.file?.path ? request.file.path : ""
+//             }
+//         }
+//     ).then( (data) => {
+//         if(data.matchedCount == 0)
+//             next(new Error("employee not found for update"))
+//         else response.status(200).json({data})
+//     })
+//     .catch(error => next(error))
+// }
+// }
+// else {
+//     next(Error);
+// }
 }
 
 exports.deleteEmployee=async (request,response,next)=>{
--- Routes/employeeRoute.js
+++ Routes/employeeRoute.js
@@ -1,5 +1,6 @@
 const express =require("express")
 const controller = require("./../Controller/employeeController")
+const searchController=require("./../Controller/searchController");
 const {checkAdminOrBadmin,checkBadminOrAdminOrEmployee,employeeOrAdmin} = require("./../Core/auth/authenticationMW");
 const validateMW = require("./../Core/validations/validateMW")
 const {validateDelArray,validatePatchArray,validatePostArray}= require("./../Core/employeeValidationArray")
@@ -14,8 +15,14 @@ router.route("/employee")
 .post(upload.single("image"),checkAdminOrBadmin,validatePostArray,validateMW,controller.addEmployee)
 .patch(upload.single("image"),checkBadminOrAdminOrEmployee,validatePatchArray,validateMW,controller.updateEmployee)
 .delete(checkAdminOrBadmin,validateDelArray,validateMW,controller.deleteEmployee)
-
-
+ 
+// router.get("/employee")
+///////////////////////search/////////////////////////////////
+router.get("/searchName/:name",searchController.getMemberByName)
+router.get("/searchEmail/:email",searchController.getMemberByEmail)
+router.get("/bookAuther/:auther",searchController.bookAuther)
+router.get("/bookPuplisher/:puplisher",searchController.bookPuplisher)
+router.get("/bookTitle/:title",searchController.bookTitle)
 
 
 module.exports = router;
