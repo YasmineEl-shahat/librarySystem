37a75320b2a0b8e26e83463d7f886d840cea4c89
YasmineEl-shahat <yasmenelshahat754@gmail.com>
2023-03-14 22:32:03 +0200

    read book

--- Routes/bookOperationRoute.js
+++ Routes/bookOperationRoute.js
@@ -2,11 +2,10 @@ const express = require("express");
 // const { validatePostArray, validatePatchArray } = require("./../Core/");
 const validateMW = require("./../Core/validations/validateMW");
 const controller = require("./../Controller/bookController");
-const bookOperationController = require("./../Controller/bookOperationController")
+const bookOperationController = require("./../Controller/bookOperationController");
 const {
   checkBadminOrAdminOrEmployeeBook,
   checkBaseAdminOremployee,
-  
 } = require("./../Core/auth/authenticationMW");
 // Upload Image
 const uploadImage = require("../helpers/uploadingImages");
@@ -16,6 +15,14 @@ const router = express.Router();
 // router
 //   .route("/book/borrow")
 //   .get(checkBadminOrAdminOrEmployeeBook, controller.borrowBooks);
-  router.route("/borrowedBooks/list/:year?/:month?")
-  .get(checkBadminOrAdminOrEmployeeBook,bookOperationController.borrowBooksList);
+router
+  .route("/borrowedBooks/list/:year?/:month?")
+  .get(
+    checkBadminOrAdminOrEmployeeBook,
+    bookOperationController.borrowBooksList
+  );
+
+router
+  .route("/book/read")
+  .post(checkBadminOrAdminOrEmployeeBook, bookOperationController.readBook);
 module.exports = router;
